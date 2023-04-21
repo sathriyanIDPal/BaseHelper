c341076f08a5f3a47e426d42c54f0d1484f97a43
sathriyanIDPal <sathriyan.aitech@id-pal.com>
2023-04-21 20:55:51 +0530

    First Commit

---
+++ BaseHelper/Sample.swift
@@ -0,0 +1,19 @@
+//
+//  Sample.swift
+//  BaseHelper
+//
+//  Created by Sathriyan on 21/04/23.
+//
+
+import Foundation
+
+public class Sample {
+    
+    public init() { }
+    
+    public func printHelper() {
+        print("==============================")
+        print("Hello World to All")
+        print("==============================")
+    }
+}
